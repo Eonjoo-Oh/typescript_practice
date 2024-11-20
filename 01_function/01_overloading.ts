7b6aa51dfea2ae25c77bde011e2accc198a98915
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-11-20 21:40:33 +0900

    overloading

--- 01_function/01_overloading.ts
+++ 01_function/01_overloading.ts
@@ -7,5 +7,33 @@ const add:Add = (a, b) => {
 	if (typeof b === "string") return a
 	return a + b;
 }
-
 //overloading is function has multiple signiture
+
+type Config = {
+	path: string,
+	state: object
+}
+type Push = {
+	(path:string):void
+	(config: Config):void
+}
+
+const push:Push = (config) => {
+	if (typeof config === "string") {console.log(config)}
+	else {
+		console.log(config.path, config.state)
+	}
+}
+
+type Add = {
+	(a: number, b: number): number
+	(a: number, b: number, c: number): number
+}
+
+const add:Add = (a, b, c?:number) => {
+	if (c) return a + b + c
+	return a + b
+}
+
+add(1, 2)
+add(1, 2, 3)
