264df5933527082f1a4a1052c59cc2415b2a2e1c
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-11-23 11:26:42 +0900

    generic

---
+++ 01_function/03_generic.ts
@@ -0,0 +1,8 @@
+type SuperPrint = <T, M>(a: T[], b:M) => T
+
+const superPrint: SuperPrint = (a) => a[0]
+
+const a = superPrint([1, 2, 3, 4], "x")
+const b = superPrint([true, false, true])
+const c = superPrint(["a", "b", "c"], false)
+const d = superPrint([1, 2, true, false, "hello"], [])
