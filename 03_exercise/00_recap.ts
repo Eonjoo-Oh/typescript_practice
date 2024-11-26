708cece04440c76470841aecb3e766195e32c131
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-11-26 14:34:27 +0900

    recap

---
+++ 03_exercise/00_recap.ts
@@ -0,0 +1,29 @@
+interface MyStorage<T> {
+	[key: string]: T
+}
+
+class LocalStorage<T> {
+	private storage: MyStorage<T> = {}
+	set(key: string, value: T) {
+		this.storage[key] = value;
+	}
+	remove(key: string){
+		delete this.storage[key]
+	}
+	get(key:string):T {
+		return this.storage[key]
+	}
+	clear(){
+		this.storage = {}
+	}
+}
+
+const stringStorage = new LocalStorage<string>()
+
+stringStorage.get("key")
+stringStorage.set("hello", "hi!")
+
+const booleanStorage = new LocalStorage<boolean>();
+
+booleanStorage.get("xxx");
+booleanStorage.set("hello", true);
